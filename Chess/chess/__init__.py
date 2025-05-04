ee5d242788c005007ac4932b9d485d23ffe6b5b8
systemshift <42102034+systemshift@users.noreply.github.com>
2025-05-04 21:27:05 +0300

    nuke, start again

--- Chess/chess/__init__.py
+++
@@ -1,4 +0,0 @@
-from .environment import ChessEnv
-from .markov_tree import MarkovTree
-
-__all__ = ['ChessEnv', 'MarkovTree']
--- MiniGrid/minigrid/__init__.py
+++
@@ -1,4 +0,0 @@
-from .environment import MiniGridEnv
-from .markov_tree import MarkovTree
-
-__all__ = ['MiniGridEnv', 'MarkovTree']
